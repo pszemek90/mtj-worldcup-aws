6aafafda150b299e3ed9919653b5645ebae1d89e
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2023-11-01 19:18:25 +0100

    log4j2.xml added, localstack with testcontainers dependency added

--- software/getMatchesByDate/src/main/java/com/mtjworldcup/mapper/MatchMapper.java
+++ software/getMatchesByDate/src/main/java/com/mtjworldcup/mapper/MatchMapper.java
@@ -3,14 +3,8 @@ package com.mtjworldcup.mapper;
 import com.mtjworldcup.model.Match;
 import com.mtjworldcup.model.MatchDto;
 
-import java.util.List;
-
 public class MatchMapper {
     public static MatchDto mapToDto(Match entity) {
         return new MatchDto(entity.getMatchId(), entity.getStartTime(), entity.getHomeTeam(), entity.getAwayTeam());
-    }
-
-    public static List<MatchDto> mapToDto(List<Match> entities) {
-        return entities.stream().map(MatchMapper::mapToDto).toList();
     }
 }
