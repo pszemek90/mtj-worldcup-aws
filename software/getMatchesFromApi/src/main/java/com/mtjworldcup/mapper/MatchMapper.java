6c74a750d3d0ea80806f90a9f98a83945e1e2ee8
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2023-11-19 08:37:32 +0100

    getMatchesFromApi - Mapper adjusted to new dynamodb structure

--- software/getMatchesFromApi/src/main/java/com/mtjworldcup/mapper/MatchMapper.java
+++ software/getMatchesFromApi/src/main/java/com/mtjworldcup/mapper/MatchMapper.java
@@ -10,9 +10,12 @@ import java.util.List;
 public class MatchMapper {
     public static Match mapToEntity(MatchDto dto) {
         Match match = new Match();
-        match.setMatchId(dto.getFixture().getId());
+        match.setPrimaryId("match-" + dto.getFixture().getId());
+        match.setSecondaryId("match-" + dto.getFixture().getId());
+        match.setDate(Instant.ofEpochSecond(dto.getFixture().getTimestamp())
+                .atZone(ZoneId.of("Europe/Warsaw")).toLocalDate());
         match.setStartTime(Instant.ofEpochSecond(dto.getFixture().getTimestamp())
-                .atZone(ZoneId.of("Europe/Warsaw")).toLocalDateTime());
+                .atZone(ZoneId.of("Europe/Warsaw")).toLocalTime());
         match.setHomeTeam(dto.getTeams().getHome().getName());
         match.setAwayTeam(dto.getTeams().getAway().getName());
         match.setHomeScore(dto.getGoals().getHome());
