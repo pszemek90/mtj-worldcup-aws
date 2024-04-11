42e510b35bf792c782b05f23f6b0bcaa247e1e95
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-04-11 07:37:48 +0200

    getOverallPool - getOverallPool module added

---
+++ software/getOverallPool/src/main/java/com/mtjworldcup/getoverallpool/Handler.java
@@ -0,0 +1,43 @@
+package com.mtjworldcup.getoverallpool;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.mtjworldcup.dynamo.dao.MatchesDao;
+import com.mtjworldcup.dynamo.model.Match;
+import org.slf4j.Logger;
+
+import java.util.Map;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public static final Logger log = org.slf4j.LoggerFactory.getLogger(Handler.class);
+
+    private final MatchesDao matchesDao;
+
+    public Handler() {
+        matchesDao = new MatchesDao();
+    }
+
+    public Handler(MatchesDao matchesDao) {
+        this.matchesDao = matchesDao;
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        try {
+            Match overallPoolRecord = matchesDao.getOverallPool();
+            int overallPool = overallPoolRecord.getPool();
+            return new APIGatewayProxyResponseEvent()
+                    .withBody(String.valueOf(overallPool))
+                    .withHeaders(Map.of("Access-Control-Allow-Origin", "http://localhost:5173"))
+                    .withStatusCode(200);
+        } catch (Exception e) {
+            log.error("Error getting overall pool. Cause: {}", e.getMessage());
+            return new APIGatewayProxyResponseEvent()
+                    .withBody("Nie udało się pobrać puli. Spróbuj ponownie później.")
+                    .withStatusCode(404);
+        }
+    }
+}
---
+++ software/getOverallPool/src/test/java/com/mtjworldcup/getoverallpool/HandlerTest.java
@@ -0,0 +1,60 @@
+package com.mtjworldcup.getoverallpool;
+
+import com.mtjworldcup.dynamo.dao.MatchesDao;
+import com.mtjworldcup.dynamo.model.Match;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HandlerTest {
+
+    private final MatchesDao mockMatchesDao = mock(MatchesDao.class);
+
+    @Test
+    void shouldReturn100_WhenOverallPoolIs100() {
+        // Given
+        Handler handler = new Handler(mockMatchesDao);
+        when(mockMatchesDao.getOverallPool()).thenReturn(prepareOverallPool(100));
+        // When
+        var response = handler.handleRequest(null, null);
+        // Then
+        Integer overallPool = Integer.valueOf(response.getBody());
+        assertEquals(100, overallPool);
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void shouldReturn200_WhenOverallPoolIs200() {
+        // Given
+        Handler handler = new Handler(mockMatchesDao);
+        when(mockMatchesDao.getOverallPool()).thenReturn(prepareOverallPool(200));
+        // When
+        var response = handler.handleRequest(null, null);
+        // Then
+        Integer overallPool = Integer.valueOf(response.getBody());
+        assertEquals(200, overallPool);
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void shouldReturn404_WhenOverallPoolIsNotAvailable() {
+        // Given
+        Handler handler = new Handler(mockMatchesDao);
+        when(mockMatchesDao.getOverallPool()).thenThrow(new RuntimeException("Error"));
+        // When
+        var response = handler.handleRequest(null, null);
+        // Then
+        assertEquals("Nie udało się pobrać puli. Spróbuj ponownie później.", response.getBody());
+        assertEquals(404, response.getStatusCode());
+    }
+
+    private Match prepareOverallPool(int overallPool) {
+        Match match = new Match();
+        match.setPool(overallPool);
+        return match;
+    }
+
+}
