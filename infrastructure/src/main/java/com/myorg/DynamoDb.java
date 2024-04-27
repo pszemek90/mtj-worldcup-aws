2bf744a77192c9957d8bec15f68d91e5037d23b2
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-04-27 07:50:57 +0200

    infrastructure - dynamodb streams added, handleFinishedMatches Lambda added, code reformat

--- infrastructure/src/main/java/com/myorg/DynamoDb.java
+++ infrastructure/src/main/java/com/myorg/DynamoDb.java
@@ -7,68 +7,54 @@ import java.util.List;
 
 public class DynamoDb {
 
-    private DynamoDb() {}
+  private DynamoDb() {}
 
-    public static TableV2 createTable(Construct scope) {
-        return TableV2.Builder.create(scope, "matches")
-                .partitionKey(Attribute.builder()
-                        .name("primary_id")
-                        .type(AttributeType.STRING)
-                        .build())
-                .sortKey(Attribute.builder()
-                        .name("secondary_id")
-                        .type(AttributeType.STRING)
-                        .build())
-                .globalSecondaryIndexes(List.of(
-                        GlobalSecondaryIndexPropsV2.builder()
-                                .partitionKey(Attribute.builder()
-                                        .name("secondary_id")
-                                        .type(AttributeType.STRING)
-                                        .build())
-                                .sortKey(Attribute.builder()
-                                        .name("primary_id")
-                                        .type(AttributeType.STRING)
-                                        .build())
-                                .indexName("getBySecondaryId")
-                                .readCapacity(Capacity.fixed(1))
-                                .writeCapacity(Capacity.autoscaled(
-                                        AutoscaledCapacityOptions.builder()
-                                                .maxCapacity(1)
-                                                .build()))
-                                .build(),
-                        GlobalSecondaryIndexPropsV2.builder()
-                                .partitionKey(Attribute.builder()
-                                        .name("date")
-                                        .type(AttributeType.STRING)
-                                        .build())
-                                .indexName("getByDate")
-                                .projectionType(ProjectionType.ALL)
-                                .readCapacity(Capacity.fixed(1))
-                                .writeCapacity(Capacity.autoscaled(
-                                        AutoscaledCapacityOptions.builder()
-                                                .maxCapacity(1)
-                                                .build()))
-                                .build(),
-                        GlobalSecondaryIndexPropsV2.builder()
-                                .partitionKey(Attribute.builder()
-                                        .name("record_type")
-                                        .type(AttributeType.STRING)
-                                        .build())
-                                .indexName("getByRecordType")
-                                .projectionType(ProjectionType.ALL)
-                                .readCapacity(Capacity.fixed(1))
-                                .writeCapacity(Capacity.autoscaled(
-                                        AutoscaledCapacityOptions.builder()
-                                                .maxCapacity(1)
-                                                .build()))
-                                .build()))
-                .billing(Billing.provisioned(ThroughputProps.builder()
-                        .readCapacity(Capacity.fixed(1))
-                        .writeCapacity(Capacity.autoscaled(
-                                AutoscaledCapacityOptions.builder()
-                                        .maxCapacity(1)
-                                        .build()))
-                        .build()))
-                .build();
-    }
+  public static TableV2 createTable(Construct scope) {
+    return TableV2.Builder.create(scope, "matches")
+        .partitionKey(Attribute.builder().name("primary_id").type(AttributeType.STRING).build())
+        .sortKey(Attribute.builder().name("secondary_id").type(AttributeType.STRING).build())
+        .globalSecondaryIndexes(
+            List.of(
+                GlobalSecondaryIndexPropsV2.builder()
+                    .partitionKey(
+                        Attribute.builder().name("secondary_id").type(AttributeType.STRING).build())
+                    .sortKey(
+                        Attribute.builder().name("primary_id").type(AttributeType.STRING).build())
+                    .indexName("getBySecondaryId")
+                    .readCapacity(Capacity.fixed(1))
+                    .writeCapacity(
+                        Capacity.autoscaled(
+                            AutoscaledCapacityOptions.builder().maxCapacity(1).build()))
+                    .build(),
+                GlobalSecondaryIndexPropsV2.builder()
+                    .partitionKey(
+                        Attribute.builder().name("date").type(AttributeType.STRING).build())
+                    .indexName("getByDate")
+                    .projectionType(ProjectionType.ALL)
+                    .readCapacity(Capacity.fixed(1))
+                    .writeCapacity(
+                        Capacity.autoscaled(
+                            AutoscaledCapacityOptions.builder().maxCapacity(1).build()))
+                    .build(),
+                GlobalSecondaryIndexPropsV2.builder()
+                    .partitionKey(
+                        Attribute.builder().name("record_type").type(AttributeType.STRING).build())
+                    .indexName("getByRecordType")
+                    .projectionType(ProjectionType.ALL)
+                    .readCapacity(Capacity.fixed(1))
+                    .writeCapacity(
+                        Capacity.autoscaled(
+                            AutoscaledCapacityOptions.builder().maxCapacity(1).build()))
+                    .build()))
+        .billing(
+            Billing.provisioned(
+                ThroughputProps.builder()
+                    .readCapacity(Capacity.fixed(1))
+                    .writeCapacity(
+                        Capacity.autoscaled(
+                            AutoscaledCapacityOptions.builder().maxCapacity(1).build()))
+                    .build()))
+        .dynamoStream(StreamViewType.NEW_IMAGE)
+        .build();
+  }
 }
--- infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -1,5 +1,7 @@
 package com.myorg;
 
+import java.util.List;
+import java.util.Map;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.apigateway.LambdaIntegration;
@@ -8,157 +10,204 @@ import software.amazon.awscdk.services.apigateway.RestApi;
 import software.amazon.awscdk.services.dynamodb.TableV2;
 import software.amazon.awscdk.services.events.CronOptions;
 import software.amazon.awscdk.services.events.Schedule;
+import software.amazon.awscdk.services.lambda.FilterCriteria;
+import software.amazon.awscdk.services.lambda.FilterRule;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.LayerVersion;
+import software.amazon.awscdk.services.lambda.StartingPosition;
+import software.amazon.awscdk.services.lambda.eventsources.DynamoEventSource;
 import software.amazon.awscdk.services.ssm.StringParameter;
 import software.constructs.Construct;
 
-import java.util.Map;
-
 public class InfrastructureStack extends Stack {
-    public InfrastructureStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+  public InfrastructureStack(final Construct scope, final String id) {
+    this(scope, id, null);
+  }
 
-    public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+  public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
+    super(scope, id, props);
 
-        LayerVersion dynamoDbLayer = Lambda.createLayer(this, "dynamo-db-layer");
-        LayerVersion worldcupCommonLayer = Lambda.createLayer(this, "worldcup-common-layer");
-        LayerVersion cognitoLayer = Lambda.createLayer(this, "cognito-layer");
+    LayerVersion dynamoDbLayer = Lambda.createLayer(this, "dynamo-db-layer");
+    LayerVersion worldcupCommonLayer = Lambda.createLayer(this, "worldcup-common-layer");
+    LayerVersion cognitoLayer = Lambda.createLayer(this, "cognito-layer");
 
-        Function getMatchesFromApi = Lambda.createLambda(this, "getMatchesFromApi", "getfromapi",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function getMatchesFromApi =
+        Lambda.createLambda(
+            this, "getMatchesFromApi", "getfromapi", dynamoDbLayer, worldcupCommonLayer);
 
-        Function getMatchesByDate = Lambda.createLambda(this, "getMatchesByDate", "getbydate",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function getMatchesByDate =
+        Lambda.createLambda(
+            this, "getMatchesByDate", "getbydate", dynamoDbLayer, worldcupCommonLayer);
 
-        Function postTypes = Lambda.createLambda(this, "postTypes", "posttypes",
-                dynamoDbLayer, worldcupCommonLayer, cognitoLayer);
+    Function postTypes =
+        Lambda.createLambda(
+            this, "postTypes", "posttypes", dynamoDbLayer, worldcupCommonLayer, cognitoLayer);
 
-        Function getResults = Lambda.createLambda(this, "getResults", "getresults",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function getResults =
+        Lambda.createLambda(this, "getResults", "getresults", dynamoDbLayer, worldcupCommonLayer);
 
-        Function getMyTypings = Lambda.createLambda(this, "getMyTypings", "getmytypings",
-                dynamoDbLayer, worldcupCommonLayer, cognitoLayer);
+    Function getMyTypings =
+        Lambda.createLambda(
+            this, "getMyTypings", "getmytypings", dynamoDbLayer, worldcupCommonLayer, cognitoLayer);
 
-        Function getAllTypings = Lambda.createLambda(this, "getAllTypings", "getalltypings",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function getAllTypings =
+        Lambda.createLambda(
+            this, "getAllTypings", "getalltypings", dynamoDbLayer, worldcupCommonLayer);
 
-        Function getOverallPool = Lambda.createLambda(this, "getOverallPool", "getoverallpool",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function getOverallPool =
+        Lambda.createLambda(
+            this, "getOverallPool", "getoverallpool", dynamoDbLayer, worldcupCommonLayer);
 
-        Function getUserProfile = Lambda.createLambda(this, "getUserProfile", "getuserprofile",
-                dynamoDbLayer, worldcupCommonLayer, cognitoLayer);
+    Function getUserProfile =
+        Lambda.createLambda(
+            this,
+            "getUserProfile",
+            "getuserprofile",
+            dynamoDbLayer,
+            worldcupCommonLayer,
+            cognitoLayer);
 
-        Function getCurrentStateFromApi = Lambda.createLambda(this, "getCurrentStateFromApi", "getcurrentstatefromapi",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function getCurrentStateFromApi =
+        Lambda.createLambda(
+            this,
+            "getCurrentStateFromApi",
+            "getcurrentstatefromapi",
+            dynamoDbLayer,
+            worldcupCommonLayer);
 
-        Function dividePool = Lambda.createLambda(this, "dividePool", "dividepool",
-                dynamoDbLayer, worldcupCommonLayer);
+    Function dividePool =
+        Lambda.createLambda(this, "dividePool", "dividepool", dynamoDbLayer, worldcupCommonLayer);
 
-        TableV2 matchesTable = DynamoDb.createTable(this);
+    Function handleFinishedMatch =
+        Lambda.createLambda(
+            this, "handleFinishedMatch", "handlefinishedmatch", dynamoDbLayer, worldcupCommonLayer);
 
-        matchesTable.grantReadWriteData(getMatchesFromApi);
-        matchesTable.grantReadData(getMatchesByDate);
-        matchesTable.grantReadWriteData(postTypes);
-        matchesTable.grantReadData(getResults);
-        matchesTable.grantReadData(getMyTypings);
-        matchesTable.grantReadData(getAllTypings);
-        matchesTable.grantReadData(getOverallPool);
-        matchesTable.grantReadData(getUserProfile);
-        matchesTable.grantReadWriteData(getCurrentStateFromApi);
-        matchesTable.grantReadWriteData(dividePool);
+    TableV2 matchesTable = DynamoDb.createTable(this);
 
-        String matchesTableName = "MATCHES_TABLE_NAME";
-        String jwksUrl = "JWKS_URL";
-        String userPoolId = "USER_POOL_ID";
-        String rapidApiKey = "RAPID_API_KEY";
-        String rapidApiHost = "RAPID_API_HOST";
-        String baseUrl = "BASE_API_URL";
+    matchesTable.grantReadWriteData(getMatchesFromApi);
+    matchesTable.grantReadData(getMatchesByDate);
+    matchesTable.grantReadWriteData(postTypes);
+    matchesTable.grantReadData(getResults);
+    matchesTable.grantReadData(getMyTypings);
+    matchesTable.grantReadData(getAllTypings);
+    matchesTable.grantReadData(getOverallPool);
+    matchesTable.grantReadData(getUserProfile);
+    matchesTable.grantReadWriteData(getCurrentStateFromApi);
+    matchesTable.grantReadWriteData(dividePool);
+    matchesTable.grantReadWriteData(handleFinishedMatch);
+    matchesTable.grantStreamRead(handleFinishedMatch);
 
-        getMatchesFromApi.addEnvironment(matchesTableName, matchesTable.getTableName());
-        getMatchesFromApi.addEnvironment(rapidApiKey, StringParameter.valueForStringParameter(this, rapidApiKey));
-        getMatchesFromApi.addEnvironment(rapidApiHost, StringParameter.valueForStringParameter(this, rapidApiHost));
-        getMatchesFromApi.addEnvironment(baseUrl, StringParameter.valueForStringParameter(this, baseUrl));
+    String matchesTableName = "MATCHES_TABLE_NAME";
+    String jwksUrl = "JWKS_URL";
+    String userPoolId = "USER_POOL_ID";
+    String rapidApiKey = "RAPID_API_KEY";
+    String rapidApiHost = "RAPID_API_HOST";
+    String baseUrl = "BASE_API_URL";
 
-        getMatchesByDate.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getMatchesFromApi.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getMatchesFromApi.addEnvironment(
+        rapidApiKey, StringParameter.valueForStringParameter(this, rapidApiKey));
+    getMatchesFromApi.addEnvironment(
+        rapidApiHost, StringParameter.valueForStringParameter(this, rapidApiHost));
+    getMatchesFromApi.addEnvironment(
+        baseUrl, StringParameter.valueForStringParameter(this, baseUrl));
 
-        postTypes.addEnvironment(matchesTableName, matchesTable.getTableName());
-        postTypes.addEnvironment(jwksUrl, StringParameter.valueForStringParameter(this, jwksUrl));
-        postTypes.addEnvironment(userPoolId, StringParameter.valueForStringParameter(this, userPoolId));
+    getMatchesByDate.addEnvironment(matchesTableName, matchesTable.getTableName());
 
-        getResults.addEnvironment(matchesTableName, matchesTable.getTableName());
+    postTypes.addEnvironment(matchesTableName, matchesTable.getTableName());
+    postTypes.addEnvironment(jwksUrl, StringParameter.valueForStringParameter(this, jwksUrl));
+    postTypes.addEnvironment(userPoolId, StringParameter.valueForStringParameter(this, userPoolId));
 
-        getMyTypings.addEnvironment(matchesTableName, matchesTable.getTableName());
-        getMyTypings.addEnvironment(jwksUrl, StringParameter.valueForStringParameter(this, jwksUrl));
-        getMyTypings.addEnvironment(userPoolId, StringParameter.valueForStringParameter(this, userPoolId));
+    getResults.addEnvironment(matchesTableName, matchesTable.getTableName());
 
-        getAllTypings.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getMyTypings.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getMyTypings.addEnvironment(jwksUrl, StringParameter.valueForStringParameter(this, jwksUrl));
+    getMyTypings.addEnvironment(
+        userPoolId, StringParameter.valueForStringParameter(this, userPoolId));
 
-        getOverallPool.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getAllTypings.addEnvironment(matchesTableName, matchesTable.getTableName());
 
-        getUserProfile.addEnvironment(userPoolId, StringParameter.valueForStringParameter(this, userPoolId));
-        getUserProfile.addEnvironment(jwksUrl, StringParameter.valueForStringParameter(this, jwksUrl));
-        getUserProfile.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getOverallPool.addEnvironment(matchesTableName, matchesTable.getTableName());
 
-        getCurrentStateFromApi.addEnvironment(matchesTableName, matchesTable.getTableName());
-        getCurrentStateFromApi.addEnvironment(rapidApiKey, StringParameter.valueForStringParameter(this, rapidApiKey));
-        getCurrentStateFromApi.addEnvironment(rapidApiHost, StringParameter.valueForStringParameter(this, rapidApiHost));
-        getCurrentStateFromApi.addEnvironment(baseUrl, StringParameter.valueForStringParameter(this, baseUrl));
+    getUserProfile.addEnvironment(
+        userPoolId, StringParameter.valueForStringParameter(this, userPoolId));
+    getUserProfile.addEnvironment(jwksUrl, StringParameter.valueForStringParameter(this, jwksUrl));
+    getUserProfile.addEnvironment(matchesTableName, matchesTable.getTableName());
 
-        dividePool.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getCurrentStateFromApi.addEnvironment(matchesTableName, matchesTable.getTableName());
+    getCurrentStateFromApi.addEnvironment(
+        rapidApiKey, StringParameter.valueForStringParameter(this, rapidApiKey));
+    getCurrentStateFromApi.addEnvironment(
+        rapidApiHost, StringParameter.valueForStringParameter(this, rapidApiHost));
+    getCurrentStateFromApi.addEnvironment(
+        baseUrl, StringParameter.valueForStringParameter(this, baseUrl));
 
-        Schedule onceADay = Schedule.cron(CronOptions.builder()
-                .hour("0")
-                .minute("30")
+    dividePool.addEnvironment(matchesTableName, matchesTable.getTableName());
+
+    handleFinishedMatch.addEnvironment(matchesTableName, matchesTable.getTableName());
+
+    handleFinishedMatch.addEventSource(
+        DynamoEventSource.Builder.create(matchesTable)
+            .startingPosition(StartingPosition.TRIM_HORIZON)
+            .filters(
+                List.of(
+                    FilterCriteria.filter(
+                        Map.of(
+                            "dynamodb",
+                            Map.of(
+                                "NewImage",
+                                Map.of(
+                                    "match_status",
+                                    Map.of("S", FilterRule.isEqual("FINISHED"))))))))
+            .batchSize(1)
+            .build());
+
+    Schedule onceADay = Schedule.cron(CronOptions.builder().hour("0").minute("30").build());
+    EventBridgeRule.createRule(this, getMatchesFromApi, onceADay, "getMatchesCron");
+
+    Schedule everyHour = Schedule.cron(CronOptions.builder().minute("15").build());
+    EventBridgeRule.createRule(this, getCurrentStateFromApi, everyHour, "getCurrentStateCron");
+
+    onceADay = Schedule.cron(CronOptions.builder().hour("0").minute("45").build());
+    EventBridgeRule.createRule(this, dividePool, onceADay, "dividePoolCron");
+
+    RestApi api = ApiGateway.createRestApi(this);
+    api.getRoot()
+        .addResource("api")
+        .addResource("results")
+        .addMethod("GET", LambdaIntegration.Builder.create(getResults).build())
+        .getResource()
+        .getParentResource()
+        .addResource("typings")
+        .addMethod("GET", LambdaIntegration.Builder.create(getMyTypings).build())
+        .getResource()
+        .getParentResource()
+        .addResource("all-typings")
+        .addMethod("GET", LambdaIntegration.Builder.create(getAllTypings).build())
+        .getResource()
+        .getParentResource()
+        .addResource("overall-pool")
+        .addMethod("GET", LambdaIntegration.Builder.create(getOverallPool).build())
+        .getResource()
+        .getParentResource()
+        .addResource("user-profile")
+        .addMethod("GET", LambdaIntegration.Builder.create(getUserProfile).build())
+        .getResource()
+        .getParentResource()
+        .addResource("matches")
+        .addResource("{date}")
+        .addMethod(
+            "GET",
+            LambdaIntegration.Builder.create(getMatchesByDate).build(),
+            MethodOptions.builder()
+                .requestParameters(Map.of("method.request.path.date", true))
+                .build())
+        .getResource()
+        .addMethod(
+            "POST",
+            LambdaIntegration.Builder.create(postTypes).build(),
+            MethodOptions.builder()
+                .requestParameters(Map.of("method.request.path.date", true))
                 .build());
-        EventBridgeRule.createRule(this, getMatchesFromApi, onceADay, "getMatchesCron");
-
-        Schedule everyHour = Schedule.cron(CronOptions.builder()
-                .minute("15")
-                .build());
-        EventBridgeRule.createRule(this, getCurrentStateFromApi, everyHour, "getCurrentStateCron");
-
-        onceADay = Schedule.cron(CronOptions.builder()
-                .hour("0")
-                .minute("45")
-                .build());
-        EventBridgeRule.createRule(this, dividePool, onceADay, "dividePoolCron");
-
-        RestApi api = ApiGateway.createRestApi(this);
-        api.getRoot()
-                .addResource("api")
-                .addResource("results")
-                .addMethod("GET", LambdaIntegration.Builder.create(getResults).build())
-                .getResource()
-                .getParentResource()
-                .addResource("typings")
-                .addMethod("GET", LambdaIntegration.Builder.create(getMyTypings).build())
-                .getResource()
-                .getParentResource()
-                .addResource("all-typings")
-                .addMethod("GET", LambdaIntegration.Builder.create(getAllTypings).build())
-                .getResource()
-                .getParentResource()
-                .addResource("overall-pool")
-                .addMethod("GET", LambdaIntegration.Builder.create(getOverallPool).build())
-                .getResource()
-                .getParentResource()
-                .addResource("user-profile")
-                .addMethod("GET", LambdaIntegration.Builder.create(getUserProfile).build())
-                .getResource()
-                .getParentResource()
-                .addResource("matches")
-                .addResource("{date}")
-                .addMethod("GET", LambdaIntegration.Builder.create(getMatchesByDate).build(),
-                        MethodOptions.builder()
-                                .requestParameters(Map.of("method.request.path.date", true))
-                                .build())
-                .getResource()
-                .addMethod("POST", LambdaIntegration.Builder.create(postTypes).build(),
-                        MethodOptions.builder()
-                                .requestParameters(Map.of("method.request.path.date", true))
-                                .build());
-    }
+  }
 }
