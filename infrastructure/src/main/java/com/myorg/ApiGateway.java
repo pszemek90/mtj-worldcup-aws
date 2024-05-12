b0dcbe640c752632a459821a8dc73d6dd5e7afb2
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-05-12 15:28:35 +0200

    infrastructure - domain added to preflight request origin

--- infrastructure/src/main/java/com/myorg/ApiGateway.java
+++ infrastructure/src/main/java/com/myorg/ApiGateway.java
@@ -7,23 +7,30 @@ import software.constructs.Construct;
 import java.util.List;
 
 public class ApiGateway {
-    private ApiGateway(){}
+  private ApiGateway() {}
 
-    public static RestApi createRestApi(Construct scope, IUserPool userPool) {
-        return RestApi.Builder.create(scope, "worldcup-api")
-                .deployOptions(StageOptions.builder()
-                        .throttlingBurstLimit(1)
-                        .throttlingRateLimit(1)
-                        .loggingLevel(MethodLoggingLevel.ERROR)
+  public static RestApi createRestApi(Construct scope, IUserPool userPool) {
+    return RestApi.Builder.create(scope, "worldcup-api")
+        .deployOptions(
+            StageOptions.builder()
+                .throttlingBurstLimit(1)
+                .throttlingRateLimit(1)
+                .loggingLevel(MethodLoggingLevel.ERROR)
+                .build())
+        .cloudWatchRole(true)
+        .defaultCorsPreflightOptions(
+            CorsOptions.builder()
+                .allowOrigins(List.of("http://localhost:5173", "https://www.mtjeuro24.pl"))
+                .build())
+        .defaultMethodOptions(
+            MethodOptions.builder()
+                .authorizationType(AuthorizationType.COGNITO)
+                .authorizer(
+                    CognitoUserPoolsAuthorizer.Builder.create(
+                            scope, "worldcup-user-pool-authorizer")
+                        .cognitoUserPools(List.of(userPool))
                         .build())
-                .cloudWatchRole(true)
-                .defaultCorsPreflightOptions(CorsOptions.builder().allowOrigins(List.of("http://localhost:5173")).build())
-                .defaultMethodOptions(MethodOptions.builder()
-                        .authorizationType(AuthorizationType.COGNITO)
-                        .authorizer(CognitoUserPoolsAuthorizer.Builder.create(scope, "worldcup-user-pool-authorizer")
-                                .cognitoUserPools(List.of(userPool))
-                                .build())
-                        .build())
-                .build();
-    }
+                .build())
+        .build();
+  }
 }
