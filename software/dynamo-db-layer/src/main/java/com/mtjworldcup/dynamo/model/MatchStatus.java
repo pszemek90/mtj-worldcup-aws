22d52a5c939ff375a27c2e7ec7741a749dd21798
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-07-20 08:04:23 +0200

    dynamo-db-layer - match status derived from long name

--- software/dynamo-db-layer/src/main/java/com/mtjworldcup/dynamo/model/MatchStatus.java
+++ software/dynamo-db-layer/src/main/java/com/mtjworldcup/dynamo/model/MatchStatus.java
@@ -3,7 +3,7 @@ package com.mtjworldcup.dynamo.model;
 public enum MatchStatus {
     SCHEDULED, IN_PROGRESS, FINISHED;
 
-    public static MatchStatus fromShortName(String shortName) {
-        return shortName.equals("FT") ? FINISHED : SCHEDULED;
+    public static MatchStatus fromLongName(String longName) {
+        return longName.equals("Match Finished") ? FINISHED : SCHEDULED;
     }
 }
